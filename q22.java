e17ffbdde78d2d1215ed0bdf4df196dad63f9bff
Tanesha Gahtori <104068017+taneshagahtori@users.noreply.github.com>
2024-09-02 20:05:18 +0530

    q22

---
+++ q22.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public static int[] getSecondOrderElements(int n, int[] a) {
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            
+            if (a[i] > largest) {
+                secondLargest = largest;
+                largest = a[i];
+            } else if (a[i] > secondLargest && a[i] < largest) {
+                secondLargest = a[i];
+            }
+
+
+            if (a[i] < smallest) {
+                secondSmallest = smallest;
+                smallest = a[i];
+            } else if (a[i] < secondSmallest && a[i] > smallest) {
+                secondSmallest = a[i];
+            }
+        }
+
+    
+        return new int[]{secondLargest, secondSmallest};
+    }
+}
